b19271b38382afd3f5578f1a557f69c65320f6f3
Vladimir Gasanov <vgasanov@hrsinternational.com>
2023-04-20 13:02:41 +0300

    add rose-pine them

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,6 +84,11 @@ _G.packer_plugins = {
     path = "/Users/vg/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["rose-pine"] = {
+    loaded = true,
+    path = "/Users/vg/.local/share/nvim/site/pack/packer/start/rose-pine",
+    url = "https://github.com/rose-pine/neovim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/vg/.local/share/nvim/site/pack/packer/start/telescope.nvim",
