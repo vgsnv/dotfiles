7c490203c51f18a10ed4d526af11c45d51c38fea
Vladimir Gasanov <vgasanov@hrsinternational.com>
2023-10-28 13:47:11 +0300

    fix telescope

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/vg/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/vg/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/vg/.cache/nvim/packer_hererocks/2.1.1696795921/share/lua/5.1/?.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.1696795921/share/lua/5.1/?/init.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.1696795921/lib/luarocks/rocks-5.1/?.lua;/Users/vg/.cache/nvim/packer_hererocks/2.1.1696795921/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/vg/.cache/nvim/packer_hererocks/2.1.1696795921/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -96,7 +96,7 @@ _G.packer_plugins = {
     url = "https://github.com/neanias/everforest-nvim"
   },
   ["github-nvim-theme"] = {
-    config = { "\27LJ\2\nõ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\vstyles\1\0\0\1\0\n\14operators\tbold\fnumbers\tbold\17conditionals\tbold\14variables\tbold\rkeywords\tbold\14functions\tbold\ntypes\tbold\rcomments\tbold\14constants\tbold\fstrings\tbold\nsetup\17github-theme\frequire\0" },
+    config = { "\27LJ\2\nõ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\vstyles\1\0\0\1\0\n\rcomments\tbold\ntypes\tbold\14constants\tbold\fstrings\tbold\14operators\tbold\fnumbers\tbold\17conditionals\tbold\14variables\tbold\rkeywords\tbold\14functions\tbold\nsetup\17github-theme\frequire\0" },
     loaded = true,
     path = "/Users/vg/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
     url = "https://github.com/projekt0n/github-nvim-theme"
@@ -212,7 +212,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: github-nvim-theme
 time([[Config for github-nvim-theme]], true)
-try_loadstring("\27LJ\2\nõ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\vstyles\1\0\0\1\0\n\14operators\tbold\fnumbers\tbold\17conditionals\tbold\14variables\tbold\rkeywords\tbold\14functions\tbold\ntypes\tbold\rcomments\tbold\14constants\tbold\fstrings\tbold\nsetup\17github-theme\frequire\0", "config", "github-nvim-theme")
+try_loadstring("\27LJ\2\nõ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\foptions\1\0\0\vstyles\1\0\0\1\0\n\rcomments\tbold\ntypes\tbold\14constants\tbold\fstrings\tbold\14operators\tbold\fnumbers\tbold\17conditionals\tbold\14variables\tbold\rkeywords\tbold\14functions\tbold\nsetup\17github-theme\frequire\0", "config", "github-nvim-theme")
 time([[Config for github-nvim-theme]], false)
 -- Config for: nvim-autopairs
 time([[Config for nvim-autopairs]], true)
